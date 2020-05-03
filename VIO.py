f0dbe696ca21a3731d17e9f777f95f48d665604c
habib-boloorchi <hboloor@okstate.edu>
2020-05-03 18:21:39 -0500

    fixed errors for monocular

--- VIO.py
+++ VIO.py
@@ -19,7 +19,7 @@ import threading
 import time
 
 class Hansel:
-    
+
     # Instantiate CvBridge
     #bridge = CvBridge()
     #df = pd.DataFrame(columns=['pointNum','x', 'y', 'patch','frameNum'])
@@ -44,9 +44,9 @@ class Hansel:
         self.IMU = []
         self.IMU_angular = []
         self.IMU_linear=[]
-        
-        
-        self.rate = rospy.Rate(rospy.get_param('~hz', 4)) 
+
+
+        self.rate = rospy.Rate(rospy.get_param('~hz', 4))
         #self.rate =rospy.Rate(10)
         rospy.Subscriber("/cam0/image_raw", Image, self.image_callback)
         rospy.Subscriber("/cam1/image_raw", Image, self.image_callbackr)
@@ -56,14 +56,14 @@ class Hansel:
         #rospy.Subscriber("/cam0/image_raw", Imu, self.image_callback)
     def savetoCSV(self):
         self.df.to_csv('file_name2', encoding='utf-8', index=False)
-        
+
         self.perceptionDF.to_csv('perception', encoding='utf-8', index=False)
         print("csv saved")
 
-    
+
 
 ##########################################find distance betweeen 2 frames
-    def distance(self, vec1, vec2): 
+    def distance(self, vec1, vec2):
 
         dist = Dist.euclidean(vec1, vec2)
         dist = np.sum(dist)
@@ -81,7 +81,7 @@ class Hansel:
         #print("y =" ),
         #print(Y)
         return [X,Y]
-          
+
 #############################################get pandas rows give be similar pairs
     def similarPatchsIn(self,currentFrameNumber,frameTocheck,df):
        # print(frameTocheck)
@@ -92,10 +92,10 @@ class Hansel:
         currentGT = currentFrame[['groundtruth']].to_numpy()
         #print ("gt = "),
         #print(currentGT[0][0][0])
-        
+
         lastPatchs = lastFrame[['patch']].to_numpy()
         lastGT = lastFrame[['groundtruth']].to_numpy()
-        
+
         GT_diff = [lastGT[0][0][0] -currentGT[0][0][0],lastGT[0][0][1] -currentGT[0][0][1],lastGT[0][0][2] -currentGT[0][0][2]]
         if GT_diff[0]==0 and GT_diff[1]==0 and GT_diff[2]==0 and self.frame >3:
             lastFrame =self.df[self.df['frameNum'] == frameTocheck-2]
@@ -115,7 +115,7 @@ class Hansel:
                 jk +=1
                 temprow = [ik,jk,self.distance(i,j)]
                 dist = np.vstack([dist,temprow])
-        
+
         # it sort the point by their distances
         dist  = dist[dist[:,2].argsort()]
         mostMatchs = dist[:5,:]
@@ -129,25 +129,17 @@ class Hansel:
         p1=[]
         p2=[]
         for i in range(5):
-            [x1,y1]=self.coordinateOf(currentFrameNumber-1,mostMatchs[i,1])
-            [x2,y2 ]= self.coordinateOf(currentFrameNumber,mostMatchs[i,0])
-            p1[i]=[x1,y1]
-            p2[i]=[x2,y2]
-
-            self.perceptionDF.loc[len(self.perceptionDF)] = [currentFrameNumber,x2-x1,y2-y1,imu_angular,imu_linear,GT_diff]
-
-  
-        
-
-             
-
+            [x1, y1] = self.coordinateOf(currentFrameNumber - 1, mostMatchs[i, 1])
+            [x2, y2] = self.coordinateOf(currentFrameNumber, mostMatchs[i, 0])
+            self.perceptionDF.loc[len(self.perceptionDF)] = [currentFrameNumber, x2 - x1, y2 - y1, imu_angular,
+                                                             imu_linear, GT_diff]
 
         ####################put data in dataframe
        # self.perceptionDF.loc[len(self.perceptionDF)] = [self.coordinateOf(currentFrameNumber-1,mostMatchs[0,1]),]
-               
-            
-        
-        
+
+
+
+
 #######################################show
     def show_Method(self,image1,image2):
         cv2.imshow('image2',image2)
@@ -178,7 +170,7 @@ class Hansel:
             # Convert your ROS Image message to OpenCV2
             self.img = self.bridge.imgmsg_to_cv2(msg, "mono8")
 
-        
+
         except CvBridgeError, e:
             print(e)
 
@@ -196,41 +188,41 @@ class Hansel:
             print(e)
     def main_loop(self):
         print("inside main loop")
-        #while(self.img==0) : 
-        #print(self.topic_is_comming)    
+        #while(self.img==0) :
+        #print(self.topic_is_comming)
 
         while not rospy.is_shutdown():
-            
-            #r.sleep() 
+
+            #r.sleep()
             #print(self.topic_is_comming)
             self.rate.sleep()
-            
+
             #rospy.Subscriber(image_topic, Image, self.image_callback)
             if self.image_is_comming == True and self.IMU_is_comming == True and self.GT_is_comming == True:
                 self.show_Method(self.img,self.imgr)
                 #####################################333
-                stereo = cv2.StereoBM_create(numDisparities=16, blockSize=50)
-                disparity = stereo.compute(self.img, self.imgr)
-                plt.imshow(disparity, 'gray')
+               # stereo = cv2.StereoBM_create(numDisparities=16, blockSize=50)
+                #disparity = stereo.compute(self.img, self.imgr)
+                #plt.imshow(disparity, 'gray')
 
-                plt.show()
+                #plt.show()
 
                 ############################################################33
                 corners = cv2.goodFeaturesToTrack(self.img,20,0.01,10)
                 cornersr = cv2.goodFeaturesToTrack(self.imgr, 20, 0.01, 10)
 
-                self.IMU = self.IMU_INIT   
+                self.IMU = self.IMU_INIT
                 self.groundtruth_for_frame =self.groundtruth
-        
+
                 corners = np.int0(corners)
-    
+
                 mask = np.zeros_like(self.img)
-                
+
                 pointNum = -1
-                for i in corners: 
-   
+                for i in corners:
+
                     x,y = i.ravel()
-                    if((x-4)>0 and (y-4)>0 and (x+4)<752 and (y+4)<480 ):                
+                    if((x-4)>0 and (y-4)>0 and (x+4)<752 and (y+4)<480 ):
                         pointNum +=1
                         patch =self.img[y-self.patchsize:y+self.patchsize,x-self.patchsize:x+self.patchsize]
                ####################put data in dataframe
@@ -241,21 +233,21 @@ class Hansel:
 
                 if (self.frame>0):
                     self.similarPatchsIn(self.frame,self.frame-1,self.df)
-        
+
 
 
      ################################## Landmark Matching
-     
+
                 self.frame += 1
 
-            
 
 
-        
 
 
-        
-        
+
+
+
+
 
 
 
