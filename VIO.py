162c496999584885916dc849217d4d42cb1e99e6
habib-boloorchi <hboloor@okstate.edu>
2020-05-23 02:11:28 -0500

    extracted translation and rotation

--- VIO.py
+++ VIO.py
@@ -31,7 +31,7 @@ class Hansel:
         self.IMU_is_comming = False
         self.GT_is_comming = False
         self.bridge = CvBridge()
-        self.df = pd.DataFrame(columns=['pointNum','x', 'y', 'patch','frameNum','groundtruth'])
+        self.df = pd.DataFrame(columns=['pointNum','x', 'y', 'patch','rotation','translation','frameNum','groundtruth'])
         self.startTime = 0
         self.landmarks = pd.DataFrame(columns=['pointNum','x', 'y','frameNum'])
         self.perceptionDF = pd.DataFrame(columns=['frameNum','difx', 'dify','imu angular velocity','imu linear velocity','groundtruth'])
@@ -44,6 +44,9 @@ class Hansel:
         self.IMU = []
         self.IMU_angular = []
         self.IMU_linear=[]
+        self.camera_matrix_right = np.array([[457.587, 0.0, 379.999], [0.0, 456.134, 255.238], [0.0, 0.0, 1.0]], dtype=np.float32)
+        self.camera_matrix_left = np.array([[458.654, 0.0, 367.215], [0.0, 457.296, 248.375],[ 0.0, 0.0, 1.0]], dtype=np.float32)
+        self.distortion_coefficient= np.array([-0.28340811, 0.07395907, 0.00019359, 1.76187114e-05, 0.], dtype=np.float32)
 
 
         self.rate = rospy.Rate(rospy.get_param('~hz', 4))
@@ -60,7 +63,8 @@ class Hansel:
         self.perceptionDF.to_csv('perception', encoding='utf-8', index=False)
         print("csv saved")
 
-
+    def find_same_patches_in_both_camera(self):
+        pass
 
 ##########################################find distance betweeen 2 frames
     def distance(self, vec1, vec2):
@@ -81,13 +85,18 @@ class Hansel:
         #print("y =" ),
         #print(Y)
         return [X,Y]
-
+    def row_extractor_by_frameNumber(self,name_of_df_column,number_of_frame):
+        return self.df[self.df[name_of_df_column] == number_of_frame]
 #############################################get pandas rows give be similar pairs
     def similarPatchsIn(self,currentFrameNumber,frameTocheck,df):
        # print(frameTocheck)
-        currentFrame = self.df[self.df['frameNum'] == currentFrameNumber]
 
-        lastFrame =self.df[self.df['frameNum'] == frameTocheck]
+        #currentFrame1 = self.df[self.df['frameNum'] == currentFrameNumber]
+        currentFrame = self.row_extractor_by_frameNumber('frameNum',currentFrameNumber)
+        #print (currentFrame)
+        #print (currentFrame1)
+        #lastFrame =self.df[self.df['frameNum'] == frameTocheck]
+        lastFrame = self.row_extractor_by_frameNumber('frameNum',frameTocheck)
         currentPatchs = currentFrame[['patch']].to_numpy()
         currentGT = currentFrame[['groundtruth']].to_numpy()
         #print ("gt = "),
@@ -159,7 +168,16 @@ class Hansel:
         self.IMU_INIT = [msg]
         #print(msg)
 
+    def findDecomposedEssentialMatrix(self, p1, p2):
+        # fundamental matrix and inliers
+        F, mask = cv2.findFundamentalMat(p1, p2, cv2.FM_LMEDS, 1, 0.999)
+        #F, mask = cv.findFundamentalMat(p1, p2, cv.FM_RANSAC, 1.0, 0.999)
+        mask = mask.astype(bool).flatten()
+        E = np.dot(self.camera_matrix_left.T, np.dot(F, self.camera_matrix_right))
 
+        _, R, t, _ = cv2.recoverPose(E, p1[mask], p2[mask], self.camera_matrix_right)
+
+        return R, t
 
     def image_callback(self,msg):
         #print("frame = "),
@@ -199,7 +217,7 @@ class Hansel:
 
             #rospy.Subscriber(image_topic, Image, self.image_callback)
             if self.image_is_comming == True and self.IMU_is_comming == True and self.GT_is_comming == True:
-                self.show_Method(self.img,self.imgr)
+                #self.show_Method(self.img,self.imgr)
                 #####################################333
                # stereo = cv2.StereoBM_create(numDisparities=16, blockSize=50)
                 #disparity = stereo.compute(self.img, self.imgr)
@@ -210,15 +228,43 @@ class Hansel:
                 ############################################################33
                 corners = cv2.goodFeaturesToTrack(self.img,20,0.01,10)
                 cornersr = cv2.goodFeaturesToTrack(self.imgr, 20, 0.01, 10)
+                cl = cv2.goodFeaturesToTrack(self.img, 20, 0.01, 10)
+                cr = cv2.goodFeaturesToTrack(self.imgr, 20, 0.01, 10)
+
 
                 self.IMU = self.IMU_INIT
                 self.groundtruth_for_frame =self.groundtruth
 
                 corners = np.int0(corners)
-
-                mask = np.zeros_like(self.img)
+                cornersr = np.int0(cornersr)
+                ####we dont need the mask
+                #mask = np.zeros_like(self.img)
+                #######################3333
 
                 pointNum = -1
+
+
+
+                ############################new approach
+                Rotation ,translation = self.findDecomposedEssentialMatrix(corners, cornersr)
+                #print(cv2.findEssentialMat(corners,cornersr,[457.587, 0.0, 379.999, 0.0, 456.134, 255.238, 0.0, 0.0, 1.0]))
+                #print (cv2.findFundamentalMat(corners,cornersr,method=cv2.RANSAC))
+                #print(cv2.findEssentialMat(corners,cornersr,[[457.587, 0.0, 379.999],[ 0.0, 456.134, 255.238],[ 0.0, 0.0, 1.0]],method=cv2.RANSAC,prob=0.99))
+                #print (self.camera_matrix_left)
+                #pts_l_norm = cv2.undistortPoints(np.expand_dims(corners, axis=1), cameraMatrix=self.camera_matrix_left, distCoeffs=self.distortion_coefficient)
+                #pts_r_norm = cv2.undistortPoints(np.expand_dims(cornersr, axis=1), cameraMatrix=self.camera_matrix_right, distCoeffs=self.distortion_coefficient)
+                #E, mask = cv2.findEssentialMat(pts_l_norm, pts_r_norm, focal=1.0, pp=(0., 0.), method=cv2.RANSAC,
+                           #                    prob=0.999, threshold=3.0)
+                #points, R, t, mask = cv2.recoverPose(E, pts_l_norm, pts_r_norm)
+
+                print ('Rotation = '),
+                print (Rotation)
+                print ('Translation = '),
+                print (translation)
+
+
+
+                ###############################################################################################33
                 for i in corners:
 
                     x,y = i.ravel()
@@ -226,12 +272,14 @@ class Hansel:
                         pointNum +=1
                         patch =self.img[y-self.patchsize:y+self.patchsize,x-self.patchsize:x+self.patchsize]
                ####################put data in dataframe
-                        self.df.loc[len(self.df)] = [pointNum,x,y,patch.flatten(),self.frame,self.groundtruth_for_frame]
+                        self.df.loc[len(self.df)] = [pointNum,x,y,patch.flatten(),Rotation,translation,self.frame,self.groundtruth_for_frame]
 
 
 
 
                 if (self.frame>0):
+
+                    #self.find_same_patches_in_both_camera()
                     self.similarPatchsIn(self.frame,self.frame-1,self.df)
 
 
